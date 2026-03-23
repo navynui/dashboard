055cb1024ce24f099d2c9a0f8ec7eece6136ca95
navynui <sandandsea@gmail.com>
2026-03-23 07:17:58 +0700

    update sync to server

--- status.php
+++ status.php
@@ -7,10 +7,10 @@ $services = [
     'qbittorrent' => 'http://192.168.31.243:8080/',
     'prowlarr' => 'http://192.168.31.243:9696/',
     'fintracker' => 'http://192.168.31.243:8000/',
-    'phpmyadmin' => 'http://192.168.31.243:8002/',
+    'dbgate' => 'http://192.168.31.243:8002/',
     'fintrackerv2' => 'http://192.168.31.243:3000/',
-    'pgadmin' => 'http://192.168.31.234:8084/',
-    'proxmox' => 'http://192.168.31.241:8006/',
+    'codeserver' => 'http://192.168.31.243:2000/',
+    'proxmox' => 'https://192.168.31.241:8006/',
     'camera' => 'http://192.168.31.244:8080/',
     'blockyui' => 'http://192.168.31.243:8081/',
     'sea' => 'http://192.168.31.244:80/',
@@ -20,12 +20,20 @@ $status = [];
 
 foreach ($services as $name => $url) {
     $ch = curl_init($url);
-    curl_setopt_array($ch, [
+    $curlOptions = [
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 3,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_MAXREDIRS => 1,
-    ]);
+    ];
+
+    // Disable SSL verification for internal HTTPS (e.g. Proxmox)
+    if (strpos($url, 'https://') === 0) {
+        $curlOptions[CURLOPT_SSL_VERIFYPEER] = false;
+        $curlOptions[CURLOPT_SSL_VERIFYHOST] = false;
+    }
+
+    curl_setopt_array($ch, $curlOptions);
     curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
